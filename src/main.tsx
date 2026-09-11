bea5713bd4a46a3a3215838bf37ba2d237b64e21
nakrch <52924317+nakrch@users.noreply.github.com>
2026-09-11 20:33:54 +0900

    style: load final studio polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './previewCompact.css'
 import './ui/feedback/toast.css'
 import './studio.css'
 import './studioPresets.css'
+import './studioPolish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
